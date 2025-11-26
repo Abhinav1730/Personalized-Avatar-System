c7311605f0fe0afd907b32a572d9ee1285779d1d
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-11-26 14:48:24 +0530

    Add default API keys and configuration file for VAPI and SIMLI

---
+++ src/config/vapi-simli-ids.ts
@@ -0,0 +1,6 @@
+// Default API keys and configuration
+// These are fallbacks if environment variables are not set
+
+export const DEFAULT_VAPI_PUBLIC_KEY = "";
+export const DEFAULT_SIMLI_API_KEY = "";
+
